95f0358de2d21554b19dfeb1c8cf5a33f453ab85
Coco <41060101+cvbenet@users.noreply.github.com>
2018-08-08 12:46:07 -0400

    Update nav.php

--- global/nav.php
+++ global/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default navbar-fixed-top">
+   <nav class="navbar navbar-default navbar-fixed-top">
       	<div class="container">
 	        <div class="navbar-header">
 	          	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -14,7 +14,6 @@
 	          	<ul class="nav navbar-nav navbar-right">
 	            		<li><a href="./archive.php">Archive</a></li>
 	          	</ul>
-
         	</div><!--/.nav-collapse -->
       	</div>
     </nav>
