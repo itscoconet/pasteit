54cb1b55e4312dc49d24d5a5d91804d9862fcccc
Coco <41060101+cvbenet@users.noreply.github.com>
2018-08-08 00:29:38 -0400

    Update footer.php

--- global/footer.php
+++ global/footer.php
@@ -1,14 +1,6 @@
+
 <footer class="footer">
       	<div class="container">
-        	<p class="text-muted">PasteIT was developed by <a href="" target="_blank">CVBE</a>, using PHP, MySQL and BootStrap 3.3</p>
+        	<p class="text-muted">PasteIT was developed by <a href="https://cvbe.net">CVBE</a>, using PHP, MySQL, <a href="https://prismjs.com/">PrismJS</a> and BootStrap 3.3 | View in <a href="https://github.com/cvbenet/pasteit" target="_blank">Github</a></p>
       	</div>
     </footer>	
-
-
-     <!-- Prism Syntax Highlighter -->
-  	<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-  	<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.0/prism.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.0/themes/prism.min.css">
-	<script src="js/prism-components.js"></script>
-	<script src="js/prism-utopia.js"></script>
-	<script src="js/prism-library-components.js"></script>
