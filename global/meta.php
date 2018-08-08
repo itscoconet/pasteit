f9b75db0a38db0cf489994e755a378c02fdbd355
Coco <41060101+cvbenet@users.noreply.github.com>
2018-08-08 00:28:55 -0400

    Update meta.php

--- global/meta.php
+++ global/meta.php
@@ -1,14 +1,22 @@
-	<meta charset="UTF-8">
+<meta charset="UTF-8">
+
 	<!-- Google Fonts-->
 	<link href="https://fonts.googleapis.com/css?family=Nunito:400,700" rel="stylesheet">
 
-
 	<!-- Custom CSS -->
-	<link rel="stylesheet" type="text/css" href="normalize.css">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<!-- Prism Syntax Highlighter JS & CSS -->
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.0/prism.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/prism.css">
+	<script src="js/prism-library-components.js"></script>
 
 	<!-- BootStrap Scripts  &  CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	
+
 	
